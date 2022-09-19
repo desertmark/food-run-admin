3edf07b85ea059c11e7daf3d3a999051152291a6
Fernando Asulay <fasulay@inno-it.es>
2022-09-19 22:30:48 +0200

    Fix test inbe

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "health!"', () => {
+      expect(appController.health()).toEqual(
+        expect.objectContaining({
+          ok: true,
+        }),
+      );
     });
   });
 });
